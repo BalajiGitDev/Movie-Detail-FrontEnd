baecbd15b3522a85c08c17e8c04a1a190439556a
BalajiGitDev <balaji.programs@gmail.com>
2024-08-26 15:08:26 +0530

    API

--- src/global.jsx
+++ src/global.jsx
@@ -1,3 +1,3 @@
 // export const API = "https://667d34b6297972455f640549.mockapi.io";
-export const API = "http://localhost:4000";
-// export const API = "https://demo-node-nine.vercel.app";
+// export const API = "http://localhost:4000";
+export const API = "https://movie-details-backend.vercel.app/";
